5b3ebc0e924b92ae30a421ffc9cede5c7bcb4a90
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2022-08-15 01:17:55 -0400

    Changed index page message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Goodbye Argo CD!'));
+app.get('/', (req, res) => res.send('Updating Argo CD!'));
 app.listen(3000, () => console.log('Server ready'));
