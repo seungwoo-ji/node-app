109318e72ca0d87d88162cf0feb2c3371b56d256
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2022-08-14 23:34:23 -0400

    Changed screen message and fixed CI typo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello Argo CD!'))
-app.listen(3000, () => console.log('Server ready'))
+app.get('/', (req, res) => res.send('Goodbye Argo CD!'));
+app.listen(3000, () => console.log('Server ready'));
