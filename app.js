b9c1c846a595c3c57fa3e44d3894f588843a391e
Seung Woo (Paul) Ji <57281026+seungwoo-ji@users.noreply.github.com>
2022-08-16 00:59:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('CI/CD is working!'));
 app.listen(3000, () => console.log('Server ready'));
